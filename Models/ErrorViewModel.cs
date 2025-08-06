341548a29034975a30477ff1be4b7fb21a1e6a40
arthurmenezes <arthurmenezespinho2008@gmail.com>
2025-08-06 10:38:33 -0300

    atividade

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace APP_web_ferias.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
